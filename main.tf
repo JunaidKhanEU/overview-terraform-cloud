fe73cd83587929518d2147c6e6423b92350635f5
Junaid <info@junaidkhan.dev>
2022-07-25 14:17:25 +0100

    first commit

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-west-1"
+}
+
+
+resource "aws_instance" "example" {
+  ami           = "ami-0d71ea30463e0ff8d"
+  instance_type = "t2.micro"
+}
